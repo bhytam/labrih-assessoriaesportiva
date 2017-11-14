6caf99b0562f6f0b8e6dcbd4e465c0c02b0358b8
Hélder Gurgel <hlgurgel@gmail.com>
2017-11-14 16:45:50 +0000

    commit - validacao do token

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,34 @@
 "use strict";
 
 module.exports = function (app) {
-    var userController = require("../controllers/userController");
+    var userController = require("../controllers/userController"),
+        jwt = require("jsonwebtoken");
     app.route("/authenticate").post(userController.authenticate);
+
+    app.use(function (req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+        if (token) {
+            jwt.verify(token, app.get('JwtSecret'), function (err, decoded) {
+                if (err) {
+                    return res.json({ 
+                        success: false, 
+                        message: 'ms-02' 
+                    });
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+
+        } else {
+            return res.status(403).send({
+                success: false,
+                message: 'ms-03'
+            });
+        }
+    });
+
+    app.route('/teste').get(function (req, res) {
+        res.json({ success: true, data: req.decoded })
+    });
 }
