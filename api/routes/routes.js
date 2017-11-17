db01e5047de1b8e21770a1e93d1f200e2de52ea5
Hélder Gurgel <hlgurgel@gmail.com>
2017-11-17 14:36:37 +0000

    commit - permitindo cors

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,6 +5,12 @@ module.exports = function (app) {
         runnerController = require("../controllers/runnerController"),
         jwt = require("jsonwebtoken");
 
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
     app.route("/authenticate").post(userController.authenticate);
 
     app.use(function (req, res, next) {
